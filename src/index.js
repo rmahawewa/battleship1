9b5d97ab5af23ed0128c80bb3392dacb192eb226
rmahawewa <rmahawewa@yahoo.com>
2025-02-04 19:35:04 +0530

    add ships to computer's field

--- src/index.js
+++ src/index.js
@@ -1,86 +1,107 @@
-import "./styles.css";
-const {Player} = require("./player.js");
-const {Ship} = require("./ship.js");
-
-
-
-let computer = Player();
-let player = Player();
-
-const gamesBoardContainer = document.querySelector('#gamesboard-container')
-const optionContainer = document.querySelector(".options-container");
-let flipButton = document.querySelector("#flip-button");
+const gamesBoardContainer = document.querySelector('#gamesBoardContainer')
+const optionContainer = document.querySelector('.option-container')
+const flipButton =  document.querySelector('#flip-button');
 
 //option choosing
-let angle = 0;
-function flip() {
-    // console.log(Array.from(optionContainer.children));
-    const optionShips = Array.from(optionContainer.children);
-    angle = angle ===  0 ? 90 : 0
+let angle = 0
+function flip(){
+    const optionShips = Array.from(optionContainer.children)
+    angle = angle === 0 ? 90 : 0
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
 
-flipButton.addEventListener("click", flip)
+flipButton.addEventListener("click", flip);
 
-
-//creating board
+//creating boards
 const width = 10
 
 function createBoard(color, user){
-    const gameBoardContainer = document.createElement('div');
-    gameBoardContainer.classList.add('game-board');
-    gameBoardContainer.style.backgroundColor = color;
+    const gameBoardContainer = document.createElement('div')
+    gameBoardContainer.classList.add('game-board')
+    gameBoardContainer.style.backgroundColor = color
     gameBoardContainer.id = user
 
-    for (let i=0; i < width * width; i++){
+
+    for(let i=0;i<width*width;i++){
         const block = document.createElement('div')
         block.classList.add('block')
-        block.id = i
+        block.id=i
         gameBoardContainer.append(block)
     }
 
 
-    gamesBoardContainer.append(gameBoardContainer);
+    gamesBoardContainer.append(gameBoardContainer)
 }
 
 createBoard('yellow', 'player')
 createBoard('pink', 'computer')
 
 
+
 //Creating Ships
-const ship1 = Ship("destroyer", 2)
-const ship2 = Ship("submarine", 3)
-const ship3 = Ship("cruiser", 3)
-const ship4 = Ship("battleship", 4)
-const ship5 = Ship("carrier", 5)
+class Ship{
+    constructor(name, length){
+        this.name = name
+        this.length = length
+    }
+}
 
-const ships = ["destroyer", "submarine", "cruiser", "battleship", "carrier"]
+const destroyer = new Ship('destroyer', 2)
+const submarine = new Ship('submarine',3)
+const cruiser = new Ship('cruiser',3)
+const battleship = new Ship('battleship',4)
+const carrier = new Ship('carrier',5)
 
-function addShipPiece(ships){  
+const ships = [destroyer,submarine,cruiser,battleship,carrier]
 
+function addShipPiece(ship){
     const allBoardBlocks = document.querySelectorAll('#computer div')
     let randomBoolean = Math.random() < 0.5
-    let isHorizontal = randomBoolean 
+    let isHorizontal = randomBoolean
     let randomStartIndex = Math.floor(Math.random() * width * width)
-    console.log(randomStartIndex)
+    
+    let validStart = isHorizontal ? randomStartIndex <= width * width - ship.length ? randomStartIndex : width * width - ship.length : 
+    //handle vertical
+    randomStartIndex <= width * width - ship.length ? randomStartIndex : randomStartIndex - ship.length * width + width
 
     let shipBlocks = []
 
-    for (let i = 0; i<ships.length; i++){
+    for(let i=0;i<ship.length;i++){
         if(isHorizontal){
-            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i])
-        } else{
-            shipBlocks.push(allBoardBlocks[Number(randomStartIndex) + i * width])
+            shipBlocks.push(allBoardBlocks[Number(validStart) + i])
+        }else{
+            shipBlocks.push(allBoardBlocks[Number(validStart)+i*width])
         }
     }
 
-    console.log(shipBlocks);
+    let valid = "";
+    if(isHorizontal){
+        shipBlocks.every((_shipBlock, index) => 
+        valid = shipBlocks[0].id % width !== width - (shipBlocks.length - (index + 1)))
+    }else{
+        shipBlocks.every((_shipBlock, index) => 
+            valid = shipBlocks[0].id < 90 + (width * index + 1)
+        )
+    }
 
-    shipBlocks.forEach(shipBlock => {
-        shipBlock.classList.add(ships.name)
-        shipBlock.classList.add('taken')
-    })
+    const notTaken = shipBlocks.every(shipBlock => !shipBlock.classList.contains('taken'))
+    
+    if(valid && notTaken){
+        shipBlocks.forEach(shipBlock => {
+            shipBlock.classList.add(ship.name)
+            shipBlock.classList.add('taken')
+        })
+    }else{
+        addShipPiece(ship)
+    }
 
+    
+    
 }
 
-addShipPiece("destroyer")
+ships.forEach(ship => addShipPiece(ship))
+
+// addShipPiece(destroyer)
+
+
+//Drag player ships
